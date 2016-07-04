3bb307e09e448d26778fa367912f58ff70978f6b
Rory Cousland <rcousland@gmail.com>
2016-07-04 17:44:05 +0100

    Update main.js

--- client/main.js
+++ client/main.js
@@ -4,9 +4,12 @@ Meteor.startup(function () {
 
 if (Meteor.isClient){
 	Template.demo.events({
+		
+		// show ip source inputs when checkbox has been selected
 		'change .js-src-limit':function(event){
 			$(".js-src-input").toggle(300);
 		},
+		// show ip destination inputs when checkbox has been selected
 		'change .js-dst-limit':function(event){
 			$(".js-dst-input").toggle(300);
 		}
